5e3d5fd217afcb1bce3e4bc0ea5c2bf0791a526c
Victor Quero <querodeveloper212@gmail.com>
2023-02-09 11:00:37 -0600

    Segundo Commit

---
+++ build/js/dashboard/dashbord.js
@@ -0,0 +1,88 @@
+var chart    = document.getElementById('chart').getContext('2d'),
+    gradient = chart.createLinearGradient(0, 0, 0, 450);
+
+gradient.addColorStop(0, 'rgba(0, 199, 214, 0.32)');
+gradient.addColorStop(0.3, 'rgba(0, 199, 214, 0.1)');
+gradient.addColorStop(1, 'rgba(0, 199, 214, 0)');
+
+
+var data  = {
+    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October', 'November','December' ],
+    datasets: [{
+			label: 'Applications',
+			backgroundColor: gradient,
+			pointBackgroundColor: '#00c7d6',
+			borderWidth: 1,
+			borderColor: '#0e1a2f',
+			data: [60, 45, 80, 30, 35, 55,25,80,40,50,80,50]
+    }]
+};
+
+var options = {
+	responsive: true,
+	maintainAspectRatio: true,
+	animation: {
+		easing: 'easeInOutQuad',
+		duration: 520
+	},
+	scales: {
+		yAxes: [{
+      ticks: {
+        fontColor: '#5e6a81'
+      },
+			gridLines: {
+				color: 'rgba(200, 200, 200, 0.08)',
+				lineWidth: 1
+			}
+		}],
+    xAxes:[{
+      ticks: {
+        fontColor: '#5e6a81'
+      }
+    }]
+	},
+	elements: {
+		line: {
+			tension: 0.4
+		}
+	},
+	legend: {
+		display: false
+	},
+	point: {
+		backgroundColor: '#00c7d6'
+	},
+	tooltips: {
+		titleFontFamily: 'Poppins',
+		backgroundColor: 'rgba(0,0,0,0.4)',
+		titleFontColor: 'white',
+		caretSize: 5,
+		cornerRadius: 2,
+		xPadding: 10,
+		yPadding: 10
+	}
+};
+
+var chartInstance = new Chart(chart, {
+    type: 'line',
+    data: data,
+		options: options
+});
+
+const btnTop = document.querySelector('.open-right-area')
+
+let menuToggleTop = document.querySelector('.open-right-area');
+
+menuToggleTop.addEventListener('click', () =>{
+        const menuTop = document.querySelector('.menu-top')
+
+        menuTop.classList.toggle('active')
+        menuToggleTop.classList.toggle('active')
+    });
+
+
+function dropMenu() {
+	const menuDrop = document.querySelector('.dropMenu')
+	
+	menuDrop.classList.toggle('active')
+}
